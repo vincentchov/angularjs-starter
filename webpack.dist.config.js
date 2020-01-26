77f9aaf0349fd4eee69605794250b5a920ecfa39
Vincent Chov <vincentchov@gmail.com>
2020-01-26 00:51:03 -0500

    fix: get rid of mangling in production webpack config

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -9,17 +9,8 @@ config.output = {
     path: path.resolve(__dirname, "dist")
 };
 
-config.plugins = config.plugins.concat([
-    // Reduces bundles total size
-    new webpack.optimize.UglifyJsPlugin({
-        mangle: {
-            // You can specify all variables that should not be mangled.
-            // For example if your vendor dependency doesn't use modules
-            // and relies on global variables. Most of angular modules relies on
-            // angular global variable, so we should keep it unchanged
-            except: ["$super", "$", "exports", "require", "angular"]
-        }
-    })
-]);
+config.optimization = {
+    mangleWasmImports: false
+};
 
 module.exports = config;
